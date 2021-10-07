f8c94a472fb31ca1faf8b75c4038e4511d4d4aa0
Dimk00z <dimk00z@gmail.com>
2021-10-07 16:48:12 +0300

    Add abstract_model

--- async_api/src/models/film.py
+++ async_api/src/models/film.py
@@ -1,20 +1,6 @@
-import orjson
-
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
-from pydantic import BaseModel
+from .abstract_model import AbstractModel
 
 
-def orjson_dumps(v, *, default):
-    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
-    return orjson.dumps(v, default=default).decode()
-
-
-class Film(BaseModel):
-    id: str
+class Film(AbstractModel):
     title: str
     description: str
-
-    class Config:
-        # Заменяем стандартную работу с json на более быструю
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
