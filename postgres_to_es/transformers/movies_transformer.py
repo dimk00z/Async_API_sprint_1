13f4e928446fdfb2fc25f9f90d25c00ef51544d7
Dimk00z <dimk00z@gmail.com>
2021-10-10 17:05:15 +0300

    Add base transformer class

--- postgres_to_es/transformers/movies_transformer.py
+++ postgres_to_es/transformers/movies_transformer.py
@@ -1,14 +1,13 @@
 from typing import List
 
+from transformers.base_transformer import BaseTransformer
 
-class Transformer:
-    def __init__(self, extracted_movies: dict) -> None:
-        self.extracted_movies = extracted_movies
 
-    def transform_movies(self) -> List[dict]:
+class MoviesTransformer(BaseTransformer):
+    def transform_data(self) -> List[dict]:
         transformed_movies: List[dict] = []
 
-        for film_work in self.extracted_movies:
+        for film_work in self.extracted_data:
             transformed_movie = {
                 "_id": str(film_work.uuid),
                 "uuid": film_work.uuid,
@@ -24,11 +23,6 @@ class Transformer:
                         {"uuid": person.uuid, "full_name": person.full_name}
                         for person in getattr(film_work, persons)
                     ]
-                    # remove actors_names, writers_names
-                    # if persons != "directors":
-                    #     transformed_movie[f"{persons}_names"] = [
-                    #         person.full_name for person in getattr(film_work, persons)
-                    #     ]
             transformed_movies.append(transformed_movie)
 
         return transformed_movies
