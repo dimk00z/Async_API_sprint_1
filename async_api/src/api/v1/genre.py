d58f4b4bb7b01eee72d4bb549df580c85da6c476
dmitrymurdashev <d.murdashev@yandex.ru>
2021-10-10 14:22:56 +1000

    Issue #9

--- async_api/src/api/v1/genre.py
+++ async_api/src/api/v1/genre.py
@@ -9,7 +9,7 @@ from services.genre import GenreService, get_genre_service
 
 router = APIRouter()
 
-persons = Optional[List[Dict[str, str]]]
+genres = Optional[List[Dict[str, str]]]
 
 
 # TODO дописать модель, реализовать выгрузку. Пока только заглушка
@@ -20,12 +20,23 @@ class Genre(BaseModel):
     name: str
 
 
-# Внедряем FilmService с помощью Depends(get_film_service)
+@router.get("/", response_model=genres)
+async def genre_list(
+        genre_service: GenreService = Depends(get_genre_service)
+) -> list[Genre]:
+    genres = await genre_service.get_all_genres()
+    if not genres:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="not one genre found")
+
+    return genres
+
+
+# Внедряем GenreService с помощью Depends(get_genre_service)
 @router.get("/{genre_uuid}", response_model=Genre)
 async def genre_details(
         genre_uuid: str, genre_service: GenreService = Depends(get_genre_service)
 ) -> Genre:
-    genre = await genre_service.get_by_id(genre_uuid)
+    genre = await genre_service.get_by_uuid(genre_uuid)
     if not genre:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="genre not found")
 
