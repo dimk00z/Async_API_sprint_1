edf0102acf8951bf1199699a4101de9306b4d980
Dimk00z <dimk00z@gmail.com>
2021-10-10 15:00:12 +0300

    Fix movies transforming

--- postgres_to_es/transformer/transformer.py
+++ postgres_to_es/transformer/transformer.py
@@ -10,22 +10,25 @@ class Transformer:
 
         for film_work in self.extracted_movies:
             transformed_movie = {
-                "_id": film_work.id,
+                "_id": str(film_work.uuid),
+                "uuid": film_work.uuid,
                 "imdb_rating": film_work.rating,
                 "title": film_work.title,
                 "description": film_work.description,
-                "genres": [genre for genre in film_work.genres],
+                "genres": [{"uuid": genre.uuid, "name": genre.name} for genre in film_work.genres],
             }
+
             for persons in ("writers", "actors", "directors"):
                 if getattr(film_work, persons):
                     transformed_movie[persons] = [
-                        {"id": person.id, "name": person.full_name}
+                        {"uuid": person.uuid, "full_name": person.full_name}
                         for person in getattr(film_work, persons)
                     ]
-                    if persons != "directors":
-                        transformed_movie[f"{persons}_names"] = [
-                            person.full_name for person in getattr(film_work, persons)
-                        ]
+                    # remove actors_names, writers_names
+                    # if persons != "directors":
+                    #     transformed_movie[f"{persons}_names"] = [
+                    #         person.full_name for person in getattr(film_work, persons)
+                    #     ]
             transformed_movies.append(transformed_movie)
 
         return transformed_movies
